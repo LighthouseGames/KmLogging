dfb916dd10c1087f710a8cf5c962ab8de4658d6a
Reed Ellsworth <ellsworthr@diamondedge.com>
2021-12-21 10:58:14 -0700

    remove unused

--- logging/src/commonMain/kotlin/org/lighthousegames/logging/MutableLogLevelController.kt
+++
@@ -1,9 +0,0 @@
-package org.lighthousegames.logging
-
-interface MutableLogLevelController : LogLevelController {
-    /**
-     * Sets the log level to a particular level.
-     * This log level and all less granular log levels will be enabled
-     */
-    fun setLogLevel(level: LogLevel)
-}
